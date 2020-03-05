7492751ca4c299ee2cd21802a99814bb866c41fc
pallavi-jtrainer <pallavi.jtrainer@gmail.com>
2020-03-05 11:40:59 +0530

    Update CarsMapper.java

--- RestServicesDemo/src/main/java/com/hexaware/persistence/CarsMapper.java
+++ RestServicesDemo/src/main/java/com/hexaware/persistence/CarsMapper.java
@@ -9,10 +9,11 @@ import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
 
 public class CarsMapper implements ResultSetMapper<Cars> {
-
+    
     @Override
     public final Cars map(final int index, final ResultSet r, final StatementContext ctx) throws SQLException {
-        return new Cars(r.getString("name"), r.getDouble("price"));
+        
+        return new Cars(r.getInt("id"), r.getString("name"), r.getDouble("price"));
     }
 
 }
