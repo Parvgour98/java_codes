29286695183b57659a0c1370e3219ecdbe70fd91
pallavi-jtrainer <pallavi.jtrainer@gmail.com>
2020-03-05 11:38:04 +0530

    Update CarsRest.java

--- RestServicesDemo/src/main/java/com/hexaware/util/CarsRest.java
+++ RestServicesDemo/src/main/java/com/hexaware/util/CarsRest.java
@@ -1,7 +1,11 @@
 package com.hexaware.util;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -21,4 +25,46 @@ public class CarsRest {
         final Cars[] carList = Cars.listAll();
         return carList;
     }
+  
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/addCar")
+    public final String addCar(final Cars car) 
+    {
+        String comment = "";
+        final int result = Cars.insertCar(car.getCarName(), car.getPrice());
+        if(result > 0){
+          comment = "{\" value \" : \" Car added successfully \"}";
+        }else{
+          comment = "{\" value \" : \" Car not added  \"}";
+        }
+        return comment;
+    }
+  
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/updateCar")
+    public final String updateCar(final Cars car) 
+    {
+        String comment = "";
+        final int result = Cars.updateCar(car);
+        if(result > 0){
+          comment = "{\" value \" : \" Car updated successfully \"}";
+        }else{
+          comment = "{\" value \" : \" Car not updated  \"}";
+        }
+        return comment;
+    }
+  
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/showCar/{id}")
+    public final Cars showCar(@PathParam("id") final int carId) {
+      final Cars car = Cars.find(carId);
+      return car;
+    }
+
+
 }
